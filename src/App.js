62959ebee913bfec6e4f5262cf66e8049cb2e55b
sabina-salehova <sabinaas@code.edu.az>
2022-08-29 08:35:14 +0400

    created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Button, Form } from "reactstrap";
+import React from "react";
+
 
 function App() {
+  const [list, setList]=React.useState([]);
+  const [name, setName]=React.useState("");
+  const [surname, setSurname]=React.useState("");
+  const [salary, setSalary]=React.useState(""); 
+
+  let setInputName=(event)=>{
+    setName(event.target.value);
+  }
+
+  let setInputSurname=(event)=>{
+    setSurname(event.target.value);
+  }
+
+  let setInputSalary=(event)=>{
+    setSalary(event.target.value);
+  }
+
+  const addEmployersListSubmit=(event)=>{
+    event.preventDefault();
+    let employer={name, surname,salary};
+    setList([...list,employer]);    
+    setName('');
+    setSurname('');
+    setSalary('');
+  }
+
+  React.useEffect(()=>{
+    localStorage.setItem('list',JSON.stringify(list));
+  },[list]);
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form className="py-4">
+        <label>Name: </label>
+        <input type="text" onChange={setInputName}/>
+
+        <label className="ps-4 pe-2">Surname: </label>
+        <input type="text" onChange={setInputSurname}/>
+
+        <label className="ps-4 pe-2">Salary: </label>
+        <input type="number" onChange={setInputSalary}/>
+
+        <Button className="ms-4 fw-bold" variant="success" onClick={addEmployersListSubmit} value={salary}>
+          +
+        </Button>
+      </Form>
     </div>
   );
 }
